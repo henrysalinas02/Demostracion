e0f90fac6065b1daabbaf94f4e7a638a8d99a0e0
henrysalinas02 <henrysalinas557@gmail.com>
2024-09-18 22:59:31 -0600

    Delete crud directory

--- crud/create.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    include "../conexion.php";
-    $query = "INSERT INTO notas (nombre, nota1, nota2, nota3, nota4) 
-    VALUES ( '".$_POST['nombre']."', '".$_POST['nota1']."', '".$_POST['nota2']."', '".$_POST['nota3']."', '".$_POST['nota4']."' )";
-    
-    $result=mysqli_query($conn,$query);
-    header ("Location: ../index.php");
-?>
